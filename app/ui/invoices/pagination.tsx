384247115d130b43da2c59a55016eea759a8fd1e
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-11-13 16:40:18 +0100

    debounce

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
+
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
   const pathname = usePathname();
